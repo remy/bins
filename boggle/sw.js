1881b1c5bf461b2a08b135bc0a15fa0ebea5c423
Remy Sharp <remy@remysharp.com>
2019-02-04 23:12:11 +0000

    WIP

--- boggle/sw.js
+++ boggle/sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v1';
+const cacheName = 'v2';
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
@@ -25,19 +25,14 @@ self.addEventListener('activate', event => {
       )
   );
 });
-
 // when the browser fetches a url, either response with
 // the cached object or go ahead and fetch the actual url
 self.addEventListener('fetch', event => {
   event.respondWith(
     // ensure we check the *right* cache to match against
     caches.open(cacheName).then(cache => {
-      return cache.match(res).then(res => {
-        if (res) {
-          return res;
-        }
-
-        return fetch(event.request);
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request);
       });
     })
   );
